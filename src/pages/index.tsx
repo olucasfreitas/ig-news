2f13d9f01808e6df7ff4f6a3d1679997b84172d0
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2023-09-05 15:08:18 -0300

    created subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,34 @@
 import Head from "next/head";
+import Image from "next/image";
+import styles from "./home.module.scss";
+import { SubscribeButton } from "@/components/SubscribeButton";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>hello world</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publication <br />
+            <span>for $9.90 a month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+        <Image
+          src="images/avatar.svg"
+          alt="girl coding"
+          width={600}
+          height={600}
+        />
+      </main>
     </>
   );
 }
